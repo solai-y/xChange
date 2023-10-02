940e0db569d534184607f8c89a46dfb87ee4c51e
solai-y <solaiy17112001@gmail.com>
2023-10-02 13:13:56 +0800

    minor comments

--- public/university.js
+++ public/university.js
@@ -7,9 +7,8 @@ function getAllUniversities() {
     })
     .then(response => {
         for (let university of response.data) {
-            console.log(university);
-            let tr = document.createElement('tr');
-            let a = document.createElement('a');
+            let tr = document.createElement('tr'); // not sure if we want to use table formatting?
+            let a = document.createElement('a'); // should set up link to the uni page (in our website) here 
             // a.setAttribute('href', university.webpages[0]);
             let p = document.createElement('p');
             let tnP = document.createTextNode(university.name, university.country);
