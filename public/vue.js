1682d2909df7c350b5e9d92ce547a7694150a42f
solai-y <solaiy17112001@gmail.com>
2023-10-11 17:26:20 +0800

    Vue JS for indiv uni page done

--- public/vue.js
+++ public/vue.js
@@ -4,19 +4,68 @@
 import { createApp, ref, watch } from 'https://cdn.jsdelivr.net/npm/vue@3.2.6/dist/vue.esm-browser.js';
 
 // VUE
-var aboutVue = ref('');
+var aboutVue = ref('Loading...');
+var universityVue = ref('University');
+var gpaVue = ref('X.X');
 
 const app = createApp({
     data() {
         return { 
             aboutVue: aboutVue.value,
+            universityVue: universityVue.value,
+            gpaVue: gpaVue.value,
         };
     },
     methods: {
         methodName() {
             // Your method logic here
+        },
+        fetchData() {
+            document.addEventListener("DOMContentLoaded", event => {
+                //retrieve get value
+                let url = window.location.search;
+                let docId = url.split("=")[1];
+                // console.log(docId);
+            
+                // retrieve data from firebase
+                if (typeof app == 'undefined') {
+                    const app = firebase.initializeApp(firebaseConfig);
+                }
+            
+                // prints the firebase connection to check for bug
+                // console.log(app);
+            
+                // check for db existence
+                if (typeof db== 'undefined') {
+                    db = firebase.firestore();
+                    auth = firebase.auth();
+                }
+            
+                // use get params to retrieve single data point form firestore
+                var userCollection = db.collection("University").doc(docId);
+                console.log(userCollection);
+
+                // Capture the 'this' context
+                const vueInstance = this;
+
+
+                userCollection.get().then((doc) => {
+                    var info = doc.data();
+                    console.log("info:", info);
+                    this.aboutVue = info.about;
+                    this.universityVue = info.name;
+                    this.gpaVue = info.gpa;
+                })
+                .catch(function(error) {
+                    console.error("Error updating document: ", error);
+                    // Handle the error, e.g., show an error message
+                });
+            });
         }
-    }
+    }, 
+    mounted() {
+        this.fetchData();
+    },
 });
 
 // Create a watcher to update the 'about' value when it changes
@@ -28,36 +77,3 @@ watch(aboutVue, (newValue) => {
 
 const vm = app.mount('#app'); 
 
-document.addEventListener("DOMContentLoaded", event => {
-    //retrieve get value
-    let url = window.location.search;
-    let docId = url.split("=")[1];
-    console.log(docId);
-
-    // retrieve data from firebase
-    if (typeof app == 'undefined') {
-        const app = firebase.initializeApp(firebaseConfig);
-    }
-
-    // prints the firebase connection to check for bug
-    // console.log(app);
-
-    // check for db existence
-    if (typeof db== 'undefined') {
-        db = firebase.firestore();
-        auth = firebase.auth();
-    }
-
-    // use get params to retrieve single data point form firestore
-    var userCollection = db.collection("University").doc(docId);
-    console.log(userCollection);
-    userCollection.get().then(function (doc) {
-        var info = doc.data();
-        console.log(info);
-        about.value = info.about;
-    })
-    .catch(function(error) {
-        console.error("Error updating document: ", error);
-        // Handle the error, e.g., show an error message
-    });
-});
