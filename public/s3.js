89ed6f7c6f7437461dbbe7a2e8ccada1c2da7359
Solaiy Meyapan <89294644+solai-y@users.noreply.github.com>
2023-10-20 08:13:19 +0800

    Update s3.js

--- public/s3.js
+++ public/s3.js
@@ -1,7 +1,5 @@
 
-const AWS_ACCESS_KEY_ID = 'AKIAXKQEJUDLDKV45O7O';
-const AWS_SECRET_ACCESS_KEY = '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';
-const AWS_REGION = 'us-east-1';
+
 
 AWS.config.update({
     accessKeyId: AWS_ACCESS_KEY_ID,
