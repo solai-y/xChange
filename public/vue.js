4574b77dc4aa4f74a062f87571ec9210e96d0e93
DyCiA13 <broccolicarrotz@gmail.com>
2023-10-25 18:23:43 +0800

    review

--- public/vue.js
+++ public/vue.js
@@ -1,96 +1,166 @@
-
 // VUE
 // Import necessary Vue Composition API functions
-import { createApp, ref, watch } from 'https://cdn.jsdelivr.net/npm/vue@3.2.6/dist/vue.esm-browser.js';
+import {
+  createApp,
+  ref,
+  watch,
+} from "https://cdn.jsdelivr.net/npm/vue@3.2.6/dist/vue.esm-browser.js";
 
 // VUE
-var aboutVue = ref('Loading...');
-var universityVue = ref('University');
-var gpaVue = ref('X.X');
-var backgroundImageVue = ref('bgimage');
-var aboutImageVue = ref('abtimage');
-var modulesVue = ref('basket');
-var galleryVue = ref('gallery');
+var aboutVue = ref("Loading...");
+var universityVue = ref("University");
+var gpaVue = ref("X.X");
+var backgroundImageVue = ref("bgimage");
+var aboutImageVue = ref("abtimage");
+var modulesVue = ref("basket");
+var galleryVue = ref("gallery");
+var userVue = JSON.parse(sessionStorage.getItem("user")).uid;
+var reviewVue = ref("review");
+var docIdVue = ref("temp");
+var comment = document.getElementById("comment");
+var username = ref("name");
+var image_url = ref("image");
 
 const app = createApp({
-    data() {
-        return { 
-            aboutVue: aboutVue.value,
-            universityVue: universityVue.value,
-            gpaVue: gpaVue.value,
-            backgroundImageVue: backgroundImageVue.value,
-            aboutImageVue: aboutImageVue.value,
-            modulesVue: modulesVue.value,
-            selectedModule: 'Managing',
-            galleryVue: galleryVue.value,
-            
-        };
+  data() {
+    return {
+      aboutVue: aboutVue.value,
+      universityVue: universityVue.value,
+      gpaVue: gpaVue.value,
+      backgroundImageVue: backgroundImageVue.value,
+      aboutImageVue: aboutImageVue.value,
+      modulesVue: modulesVue.value,
+      selectedModule: "Managing",
+      galleryVue: galleryVue.value,
+      userVue: userVue,
+      reviewVue: reviewVue.value,
+      docIdVue: docIdVue.value,
+      comment: comment.value,
+      username: username.value,
+      image_url: image_url.value,
+    };
+  },
+  methods: {
+    reviewForm: function () {
+      var popup = document.getElementById("reviewPopup");
+      popup.style.display = "block";
     },
-    methods: {
-        methodName() {
-            // Your method logic here
 
-        
-        },
-        fetchData() {
-            document.addEventListener("DOMContentLoaded", event => {
-                //retrieve get value
-                let url = window.location.search;
-                let docId = url.split("=")[1];
-                // console.log(docId);
-            
-                // retrieve data from firebase
-                if (typeof app == 'undefined') {
-                    const app = firebase.initializeApp(firebaseConfig);
-                }
-            
-                // prints the firebase connection to check for bug
-                // console.log(app);
-            
-                // check for db existence
-                if (typeof db== 'undefined') {
-                    db = firebase.firestore();
-                    auth = firebase.auth();
-                }
-            
-                // use get params to retrieve single data point form firestore
-                var userCollection = db.collection("University").doc(docId);
-                console.log(userCollection);
+    closePopup: function () {
+      var popup = document.getElementById("reviewPopup");
+      popup.style.display = "none";
+    },
 
-                // Capture the 'this' context
-                const vueInstance = this;
-
-
-                userCollection.get().then((doc) => {
-                    var info = doc.data();
-                    console.log("info:", info);
-                    this.aboutVue = info.about;
-                    this.universityVue = info.name;
-                    this.gpaVue = info.gpa;
-                    this.backgroundImageVue = info.gallery[0];
-                    this.aboutImageVue = info.gallery[1];
-                    this.modulesVue = info.modules;
-                    this.galleryVue = info.gallery;     
-                })
-                .catch(function(error) {
-                    console.error("Error updating document: ", error);
-                    // Handle the error, e.g., show an error message
-                });
-            });
+    submitReview: function () {
+      var uid = this.userVue;
+      // Save review to Firebase Firestore
+      // use get params to retrieve single data point form firestore
+      var userData = db.collection("Users").doc(uid);
+      userData.get().then(function (doc) {
+        let userInfo = doc.data;
+        this.username = userInfo.name;
+        if (typeof userInfo.image_url != "undefined") {
+          this.image_url = userInfo.image_url;
+        } else {
+          this.image_url = "./images/profile photo.jpeg";
         }
-        
+      });
+      //   console.log(userData);
+
+      // Capture the 'this' context
+      //   const vueInstance = this;
+
+      //   userData.get().then((doc) => {
+      //     let x = doc.data;
+      //     console.log(x);
+      //     this.username = doc.data().name;
+      //     if (typeof doc.data.image_url != "undefined") {
+      //       this.image_url = doc.data().image_url;
+      //     } else {
+      //       this.image_url = "./images/profile photo.jpeg";
+      //     }
+      //   });
+      db.collection("University")
+        .doc(this.docIdVue)
+        .update({
+          review: firebase.firestore.FieldValue.arrayUnion({
+            username: this.username,
+            image_url: this.image_url,
+            comment: this.comment,
+          }),
+        });
+
+      // Clear form inputs
+      this.comment = "";
+
+      // Hide the form after submission
+      this.closePopup();
     },
-    mounted() {
-        this.fetchData();
+    methodName() {
+      // Your method logic here
     },
+    fetchData() {
+      document.addEventListener("DOMContentLoaded", (event) => {
+        //retrieve get value
+        const url = window.location.search;
+        const docId = url.split("=")[1];
+
+        // console.log(docId);
+
+        // retrieve data from firebase
+        if (typeof app == "undefined") {
+          const app = firebase.initializeApp(firebaseConfig);
+        }
+
+        // prints the firebase connection to check for bug
+        // console.log(app);
+
+        // check for db existence
+        if (typeof db == "undefined") {
+          db = firebase.firestore();
+          auth = firebase.auth();
+        }
+
+        // use get params to retrieve single data point form firestore
+        var userCollection = db.collection("University").doc(docId);
+
+        console.log(userCollection);
+
+        // Capture the 'this' context
+        const vueInstance = this;
+
+        userCollection
+          .get()
+          .then((doc) => {
+            var info = doc.data();
+            console.log("info:", info);
+            this.aboutVue = info.about;
+            this.universityVue = info.name;
+            this.gpaVue = info.gpa;
+            this.backgroundImageVue = info.gallery[0];
+            this.aboutImageVue = info.gallery[1];
+            this.modulesVue = info.modules;
+            this.galleryVue = info.gallery;
+            this.reviewVue = info.review;
+            this.docIdVue = docId;
+          })
+          .catch(function (error) {
+            console.error("Error updating document: ", error);
+            // Handle the error, e.g., show an error message
+          });
+      });
+    },
+  },
+
+  mounted() {
+    this.fetchData();
+  },
 });
 
 // Create a watcher to update the 'about' value when it changes
 watch(aboutVue, (newValue) => {
-    app.config.globalProperties.aboutVue = newValue;
+  app.config.globalProperties.aboutVue = newValue;
 });
+// component must be declared before app.mount(...)
 
-
-
-const vm = app.mount('#app'); 
-
+const vm = app.mount("#app");
