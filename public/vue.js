a635f89f8803347e9d3d99b9170a2ff0f8cd5a7c
MoSengT <moseng.tim.2021@smu.edu.sg>
2023-10-31 11:52:04 +0800

    Individual Uni Google Map Vanilla

--- public/vue.js
+++ public/vue.js
@@ -29,6 +29,7 @@ const app = createApp({
     return {
       aboutVue: aboutVue.value,
       universityVue: universityVue.value,
+      // universityURL: "https://www.google.com/maps/embed/v1/search?key=AIzaSyALAMytvRCzbd_txEXw0QQwJh0GzSINQt0&q="+universityVue.value + "&zoom=15&maptype=satellite",
       gpaVue: gpaVue.value,
       backgroundImageVue: backgroundImageVue.value,
       aboutImageVue: aboutImageVue.value,
@@ -43,12 +44,17 @@ const app = createApp({
       image_url: image_url.value,
       usernameComment: "",
       imageComment: "",
+      
     };
   },
   methods: {
     reviewForm: function () {
       var popup = document.getElementById("reviewPopup");
+      
       popup.style.display = "block";
+    },
+    universityURL(){
+      return "https://www.google.com/maps/embed/v1/search?key=AIzaSyALAMytvRCzbd_txEXw0QQwJh0GzSINQt0&q="+ this.universityVue + "&zoom=15&maptype=satellite";
     },
 
     closePopup: function () {
@@ -94,7 +100,9 @@ const app = createApp({
       
     methodName() {
       // Your method logic here
+
     },
+
     fetchData() {
       document.addEventListener("DOMContentLoaded", (event) => {
         //retrieve get value
@@ -139,6 +147,7 @@ const app = createApp({
             this.galleryVue = info.gallery;
             this.reviewVue = info.review;
             this.docIdVue = docId;
+            
           })
           .catch(function (error) {
             console.error("Error updating document: ", error);
@@ -160,3 +169,4 @@ watch(aboutVue, (newValue) => {
 // component must be declared before app.mount(...)
 
 const vm = app.mount("#app");
+
